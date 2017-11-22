abf9f318ea3ecbe53b7a95ef1f112979ee15a010
Max Waine <bobishmax@hotmail.com>
2017-11-22 18:32:20 +0000

    Add licence, copyright headers

--- source/draw32.cpp
+++ source/draw32.cpp
@@ -1,4 +1,23 @@
-// -- 32-bit rendering functions --
+//
+// Copyright(C) 2007-2017 Stephen McGranahan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/
+//
+// Purpose: 32-bit rendering functions
+// Authors: Stephen McGranahan
+//
+
 #include <SDL.h>
 #include <math.h>
 #include "render.h"
